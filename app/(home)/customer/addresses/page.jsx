3fae427c5dd6f8d2057c2704748be7c2ef871e07
info.aliahmadi <info.aliahmadi@gmail.com>
2024-08-16 23:41:06 +0330

    fix bug

--- app/(home)/customer/addresses/page.jsx
+++ app/(home)/customer/addresses/page.jsx
@@ -11,22 +11,6 @@ import WhatsApp from '@mui/icons-material/WhatsApp';
 import Place from '@mui/icons-material/Place';
 // import WaveContactImage from 'assets/images/wave-contact-page.svg';
 
-export async function generateMetadata({ params }) {
-  return {
-    title: 'Contact Us',
-    description: 'We deliver the best web solutions',
-    category: 'World-Class Web Development',
-
-    openGraph: {
-      title: 'Contact Us',
-      description: 'World-Class Web Development'
-    },
-    twitter: {
-      title: 'Contact Us',
-      description: 'World-Class Web Development'
-    }
-  };
-}
 export default async function Page({ params }) {
   return (
     <>
